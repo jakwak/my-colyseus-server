9a3f417116848c74177633e7a7413404f10456cd
Ondalssam <ondalssam@gmail.com>
2025-05-15 13:42:44 +0900

    ...

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -6,6 +6,13 @@ export class MyRoom extends Room {
   state = new MyState();
 
   onCreate(options: any) {
+    console.log("MyRoom created!", options);
+
+    this.onMessage("message_type", (client, data) => {
+        console.log("MyRoom received message from", client.sessionId, ":", data);
+        // this.state.movePlayer(client.sessionId, data);
+        this.broadcast("message_type", `(${client.sessionId}) ${data}`);
+    });
   }
 
   onJoin(client: Client, options: any) {
