10d1272b55f62958d19d96ed5412dc10a173dac3
jakwak <ondalssam@gmail.com>
2025-07-27 10:44:27 +0900

    사용자 클래스에 ID 필드 추가 및 QRoom의 최대 클라이언트 수 설정; 클라이언트가 입장할 때 사용자 이름 필수 검증 추가

--- src/rooms/qna/MyState.ts
+++ src/rooms/qna/MyState.ts
@@ -1,8 +1,17 @@
 import { Schema, type, MapSchema } from "@colyseus/schema";
 
 export class User extends Schema {
+  @type("string") id: string = '';
   @type("string") username: string = '';
   @type("number") answer_number: number = 0;
+
+  constructor(data?: { username?: string; id?: string }) {
+    super();
+    if (data) {
+      this.username = data.username || '';
+      this.id = data.id || '';
+    }
+  }
 }
 
 export class MyState extends Schema {
--- src/rooms/qna/QRoom.ts
+++ src/rooms/qna/QRoom.ts
@@ -1,25 +1,39 @@
-import { Client, Room } from "@colyseus/core";
-import { MyState, User } from "./MyState";
+import { Client, Room } from '@colyseus/core'
+import { MyState, User } from './MyState'
 
 export class QRoom extends Room {
-  onCreate(options: any) {
-    this.state = new MyState();
-  }
+  maxClients = 20 // 최대 클라이언트 수 설정
   
-  onJoin(client: Client<any, any>, options?: any, auth?: any): void | Promise<any> {
-    console.log('onJoin options--->', options)
+  onCreate(options: any) {
+    this.state = new MyState()
+    this.setSeatReservationTime(15)
+  }
+
+  onJoin(
+    client: Client<any, any>,
+    options?: any,
+    auth?: any
+  ): void | Promise<any> {
+    if (!options?.username) {
+      client.send('error', { message: 'Username is required' })
+      return
+    }
+    
     client.send('__playground_message_types', {
       message: 'Hello from server',
     })
+    
     if (options.username === '선생님') {
       this.state.teacher_ready = true
-    } else {
-      this.state.users.set(client.sessionId, new User({ username: options.username }))
     }
+    
+    this.state.users.set(
+      client.sessionId,
+      new User({ username: options.username, id: client.sessionId })
+    )
   }
 
   onLeave(client: Client<any, any>, consented?: boolean): void | Promise<any> {
-    console.log('onLeave consented--->', consented)
     if (this.state.users.get(client.sessionId)) {
       this.state.users.delete(client.sessionId)
     }
